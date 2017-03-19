c498f17ab7cd6e26e955a2d40b3bb90bded15156
Jawadur Rashid <jawadur.rashid@gmail.com>
2017-03-19 14:04:39 -0400

    Updated features and code

--- app/src/main/java/jawadurrashid/top10downloaded/MainActivity.java
+++ app/src/main/java/jawadurrashid/top10downloaded/MainActivity.java
@@ -1,17 +1,115 @@
 package jawadurrashid.top10downloaded;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        while (true){                  // Loop prevents onCreate method from finishing; will make app freeze
+
+        /* while (true){                  // Loop prevents onCreate method from finishing; will make app freeze
             int x = 5;
-        }
+        }*/
+
+        // Downloading data over internet can unreliable, therefore it is good to run download on a separate thread
+
+        Log.d(TAG, "onCreate: starting Asynctask");     //To start background task, we must create instance of DownloadData class and call excecute method
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+        Log.d(TAG, "onCreate: done");
+
     }
-}
+
+    private class DownloadData extends AsyncTask<String, Void, String> {        //Subclassing AsyncTask; built in Android task that takes care of multi-threading complexities
+
+        // Allows user to run code in background thread so that app is not blocked or waiting for a long running process such as downloading data
+        // Allows you to perform background operations and publish results on the UI thread without having to manipulate threads and/or handlers.
+        // Parameters; (String) containing URL and Address of the RSS feed, (Void) progress bar not necessary for short downloads, (String) containing XML after being downloaded
+
+        private static final String TAG = "DownloadData";
+
+        @Override
+        protected void onPostExecute(String s) {          //Runs on main UI thread once the background process is completed and returns value
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: paramater is " + s);  //Print out parameter passed in
+
+            ParseApplication parseApplication = new ParseApplication(); //Creating new ParseApplication object
+            parseApplication.parse(s);  //Recall "s" is the XML data that Android framework has sent after downloading in the doInBackground method
+        }
+
+        @Override
+        protected String doInBackground(String... params) {   //Main method that does processing on other thread, elipses allows user to pass in multiple values/parameters (URL's in this case) into the class
+            Log.d(TAG, "doInBackground: starts with " + params[0]);  //Print out parameter that is passed
+            String rssFeed = downloadXML(params[0]);   //downloadXML will download the feed and return a string containing the XML
+            if (rssFeed == null) {
+                Log.e(TAG, "doInBackground: Error downloading");  //Log.d entries don't appear in the log cat when productive version of app is released, we want this message to remain therefore log.e is used
+            }
+            return rssFeed; //Elipses parameter gets passed into class as an array, wil return first element in array (params[0])
+        }
+
+        //Open http connection will access stream of data coming from the internet from the URL, connection provides input stringtherefore an input string reader will be used to read data
+        //Buffered reader - buffers data coming in from stream, read in to the buffer in memory and program could read from the buffer
+
+        private String downloadXML(String urlPath) {
+            StringBuilder xmlResult = new StringBuilder(); //Will be appending to string a lot as we read characters from the input string
+
+            try {     //Dealing with data from external source; not in computer's memory therefore data not entirely accountable
+                //Rap up a section of code and catch any exceptions that occur while it is executing
+                URL url = new URL(urlPath);   //Checked exceptions must be handled in order for compiler to work; run time exceptions don't interfere with compiling
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //iO exception can be thrown, there may be a problem with the internet connection or URl can refer to a server that does not exit
+
+                int response = connection.getResponseCode();
+                Log.d(TAG, "downloadXML: The response code was " + response);             //Input stream is created from open http connection
+
+               /* InputStream inputStream = connection.getInputStream();                    //Creates input stream, uses that to create input stream reader which is used to create a buffer reader
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream); //InputStreamReader uses inputStream object as a source of its data
+                BufferedReader reader = new BufferedReader(inputStreamReader);     //Buffered reader will be used to read the XML*/
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                int charactersRead;
+                char[] inputBuffer = new char[1000];      //InputBuffer character array (stores 1000 characters)
+                while (true) {                              //Will continue until input stream is finished
+                    charactersRead = reader.read(inputBuffer);
+                    if (charactersRead < 0) {              //Signals end of stream of data, break out of loop, link terminates and closes the buffered reader
+                        break;
+                    }
+                    if (charactersRead > 0) {              //charactersRead variable will hold and count number of characters read from stream
+                        xmlResult.append(String.copyValueOf(inputBuffer, 0, charactersRead));
+                    }
+                }
+
+                reader.close();  //Closes buffered reader, input stream reader and input stream
+                return xmlResult.toString();
+
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "downloadXML: Invalid URL" + e.getMessage()); //Error, not just debugging information
+            } catch (IOException e) {                                  //"e" is type of error, and getMessage gives us information about error
+                Log.e(TAG, "downloadXML: IO Exception reading data: " + e.getMessage()); //Order of catching exceptions matters; MalformedURLException is a sublass of the IOException
+            } catch (SecurityException e) {
+                Log.e(TAG, "downloadXML: Security exception. Needs permission? " + e.getMessage());
+//                 e.printStackTrace(); //Get access to stack trace having caught the actual exception that caused the initial stack trace
+            }
+
+            return null;
+        }
+
+    }
+
+    }
+
---
+++ app/src/main/java/jawadurrashid/top10downloaded/ParseApplication.java
@@ -0,0 +1,115 @@
+package jawadurrashid.top10downloaded;
+
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+
+/**
+ * Created by User on 2017-03-18.
+ */
+
+public class ParseApplication {
+    private static final String TAG = "ParseApplication";
+
+    //Building list of feed entry objects in class, stores applications found in XML data
+    // Will be created as we parse those entry tags in XML and will be stored in arraylist
+
+    private ArrayList<FeedEntry> applications;
+
+    public ParseApplication() {
+        this.applications = new ArrayList<>();   //When XML is parsed, list will contain various feed entry objects (one for each entry in the XML)
+    }
+
+    public ArrayList<FeedEntry> getApplications() {
+        return applications;
+    }
+
+    //Will parse and manipulate XML data Strings and create list of applications that will be stored in the application array list
+
+    public boolean parse(String xmlData) {
+        boolean status = true;
+        FeedEntry currentRecord = null;
+        boolean inEntry = false;   //Return false if data could not be parsed for whatever reason, keeps track of tags whether in entry or not in order to avoid confusion
+        String textValue = "";
+
+        try {
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();  //Responsible for setting up the Java XML parser
+            factory.setNamespaceAware(true);        //API provides factory that will produce pullParser object, factory classes are used if actual class is not known
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new StringReader(xmlData)); //Tells object what to parse (StringReader) that uses the XML string data (XML data that is downloaded from iTunes website)
+            int eventType = xpp.getEventType();  //Processes strings
+
+            while (eventType != XmlPullParser.END_DOCUMENT) { //Check if parser is at the end of the document, will loop until it gets to the end of the document
+
+                String tagName = xpp.getName();          //Checking for tags and extracting the data we need from the XML
+
+                //For example; <updated> 2016-07-21T19 </updated>
+                // <updated> is the entry or start tag, (2016-07-21) is the text value and </updated> is the end tag
+
+                switch (eventType) {                     //Getting name of current tag
+                    case XmlPullParser.START_TAG:
+                        Log.d(TAG, "parse: Starting tag for  " + tagName);
+                        if ("entry".equalsIgnoreCase(tagName)) {      //Only interested in an entry tag; only concerned with data in individual entries
+                            inEntry = true;                          //If we have entry tag we create an instance of the feed entry class for data storage
+                            currentRecord = new FeedEntry();
+                        }
+
+                        break;
+
+                    case XmlPullParser.TEXT:       //Pull parser is telling us that data is available, therefore will store the data into the String variable
+                        textValue = xpp.getText(); //Stores text when new text is available
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        Log.d(TAG, "parse: Ending tag for " + tagName); //Will read current tag and extract the appropriate data from XML according to tag
+                        if (inEntry) {                                   //Checks if pull parser is in entry tag, if it is we can test the tag name and assign the variable to the correct field of the current object
+                            if ("entry".equalsIgnoreCase(tagName)) {     //String entry cannot be null
+                                applications.add(currentRecord);        //If we reached the tagName, we have reached the end of the data for the current record (end tag for the entry)
+                                inEntry = false;
+                            } else if ("name".equalsIgnoreCase(tagName)) {  //Otherwise, the parser will store data for the various different fields
+                                currentRecord.setName(textValue);
+                            } else if ("artist".equalsIgnoreCase(tagName)) {
+                                currentRecord.setArtist(textValue);
+                            } else if ("releaseDate".equalsIgnoreCase(tagName)) {
+                                currentRecord.setReleaseDate(textValue);
+                            } else if ("summary".equalsIgnoreCase(tagName)) {
+                                currentRecord.setSummary(textValue);
+                            } else if ("image".equalsIgnoreCase(tagName)) {
+                                currentRecord.setImageURL(textValue);
+                            }
+                        }
+
+                        break;
+
+                    default: //Nothing
+                }
+
+                eventType = xpp.next();   //Tells parser to continue working in XML until something interesting happens (value in tag, reaches end of document, etc)
+
+            }
+
+            //Loop through application list once the XMl has been processed and print out values of the fields
+
+            for (FeedEntry app : applications) {
+                Log.d(TAG, "parse: ******");
+                Log.d(TAG, app.toString());
+            }
+
+        } catch (Exception e) {  //Catch all exceptions
+            status = false;
+            e.printStackTrace();
+        }
+
+        return status;
+
+    }
+
+}
+
+
+
